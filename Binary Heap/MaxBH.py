42584d37304dd8d994b856a91ceac817642a8f8c
Shahed1998 <shahedchowdhury120@gmail.com>
2023-04-01 14:49:11 +0600

    deletion max binary heap

--- Binary Heap/MaxBH.py
+++ Binary Heap/MaxBH.py
@@ -18,17 +18,47 @@ class MaxBinaryHeap:
             cIdx = pIdx
     
     def extractMax(self):
-        return self
-
-        
+        swap = self.values.pop()
+        ret = self.values[0]
+        self.values[0] = swap
+        print(self.values)
+        self.bubbleDown()
+        print(self.values)
+        return ret
+    
+    def bubbleDown(self):
+        swap = 0
+        while(swap < len(self.values)/2-1):
+            lci = 2*swap+1 # lci = left child index
+            rci = 2*swap+2 # rci = right child index
+            swapVal = self.values[swap]
+            leftVal = self.values[lci]
+            rightVal = self.values[rci]
+            if swapVal < leftVal and swapVal < rightVal:
+                if leftVal < rightVal:
+                    self.values[swap] = rightVal
+                    self.values[rci] = swapVal
+                    swap = rci
+                elif leftVal > rightVal:
+                    self.values[swap] = leftVal
+                    self.values[lci] = swapVal
+                    swap = lci
+            elif swapVal < leftVal:
+                self.values[swap] = leftVal
+                self.values[lci] = swapVal
+                swap = lci
+            elif swapVal < rightVal:
+                self.values[swap] = rightVal
+                self.values[rci] = swapVal
+                swap = rci
 
 mbh = MaxBinaryHeap()
-mbh.insert(41)
+mbh.insert(55)
 mbh.insert(39)
 mbh.insert(33)
 mbh.insert(18)
 mbh.insert(27)
 mbh.insert(12)
-mbh.insert(55)
-print(mbh.values)
+# mbh.insert(55)
+print(mbh.extractMax())
 
