c9d9bafd6be145a2667310025c33e30decb52fb7
Shahed1998 <shahedchowdhury120@gmail.com>
2023-05-24 01:46:25 +0600

    Hash Table set, get, keys, and values method done

--- Hash Table/HT.py
+++ Hash Table/HT.py
@@ -1,10 +1,10 @@
 class HashTable:
-    def __init__(self, size=53):
+    def __init__(self, size=7):
         self.keyMap = [None] * size
 
     def hash(self, key):
         total = 0
-        prime = 17
+        prime = 19
         for i in range(0, min(len(key), 100)):
             char = key[i]
             # gets character position in alphabet table
@@ -17,10 +17,49 @@ class HashTable:
         # checks whether anything is already stored in the index
         if not self.keyMap[index]:
             self.keyMap[index] = []
-        self.keyMap[index].append([key, value])
+            self.keyMap[index].append([key, value])
+        # if duplicate key is inserted overwrite the previous value
+        else:
+            duplicate = 0
+            for i in self.keyMap[index]:
+                if i[0] == key:
+                    i[1] = value
+                    duplicate = 1
+            if duplicate == 0:
+                self.keyMap[index].append([key, value])
+
+    def get(self, key):
+        index = self.hash(key)
+        if self.keyMap[index]:
+            for i in range(len(self.keyMap[index])):
+                if self.keyMap[index][i][0] == key:
+                    return self.keyMap[index][i][1]
+        return None
+
+    def keys(self):
+        keys = []
+        for i in self.keyMap:
+            if i == None:
+                continue
+            for j in i:
+                keys.append(j[0])
+        return keys
+
+    def values(self):
+        values = []
+        for i in self.keyMap:
+            if i == None:
+                continue
+            for j in i:
+                if j[1] in values:  # removes duplicate values
+                    continue
+                values.append(j[1])
+        return values
 
 
 ht = HashTable()
-ht.set("Hello", "World")
-ht.set("bye", "World")
-print(ht.keyMap)
+ht.set("Hello", "World 🍻")
+ht.set("Hello", "bye 😥")
+ht.set("bye", "World 🤞")
+print(ht.keys())
+print(ht.values())
