df28591f4118446143040ccf211792af556ba516
Shahed1998 <shahedchowdhury120@gmail.com>
2023-08-04 14:55:31 +0600

    updated Stack operation

--- Stack+Queue/Stack.py
+++ Stack+Queue/Stack.py
@@ -1,46 +1,76 @@
-class Node:
+# ------------------------------------------------------------
+# Stack using Linked List
+# -------------------------------------------------------------
 
-    def __init__(self, val):
-        self.value = val
-        self.next = None
+# class Node:
+
+#     def __init__(self, val):
+#         self.value = val
+#         self.next = None
+
+# class Stack:
+
+#     def __init__(self):
+#         self.first = None
+#         self.last = None
+#         self.size = 0
+
+#     def push(self, val):
+#         node = Node(val)
+#         if self.size == 0:
+#             self.first = node
+#             self.last = self.first
+#         else:
+#             node.next = self.first
+#             self.first = node
+#         self.size += 1
+#         return self.size
+
+#     def pop(self):
+#         if self.size == 0:
+#             return None
+#         removed = self.first
+#         if self.size == 1:
+#             self.first = None
+#         else:
+#             self.first = removed.next
+#             removed.next = None
+#         self.size -= 1
+#         return removed.value
+
+# ------------------------------------------------------------
+# Stack using Array
+# -------------------------------------------------------------
 
 class Stack:
 
     def __init__(self):
-        self.first = None
-        self.last = None
-        self.size = 0
+        self.stack = []
 
     def push(self, val):
-        node = Node(val)
-        if self.size == 0:
-            self.first = node
-            self.last = self.first
-        else:
-            node.next = self.first
-            self.first = node 
-        self.size += 1
-        return self.size
+        self.stack.append(val)
 
     def pop(self):
-        if self.size == 0:
-            return None
-        removed = self.first
-        if self.size == 1:
-            self.first = None
-        else:
-            self.first = removed.next
-            removed.next = None
-        self.size -= 1
-        return removed.value
+        return self.stack.pop()
 
-# stack = Stack()
-# stack.push("A")
-# stack.push("B")
-# stack.push("C")
-# print(stack.size)
-# print(stack.pop())
-# print(stack.pop())
-# print(stack.pop())
-# print(stack.size)
+    def peek(self):
+        if len(self.stack) > 0:
+            return self.stack[len(self.stack)-1]
+        return
 
+    def display(self):
+        return self.stack
+
+# ------------------------------------------------------------
+# Test cases
+# -------------------------------------------------------------
+
+# s = Stack()
+# s.push("A")
+# s.push("B")
+# s.push("C")
+# print(s.display())
+# s.pop()
+# s.pop()
+# s.pop()
+# print(s.display())
