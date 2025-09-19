61ba7f0f6c4f0417fc933f7bb3c3f286c4a718f9
Shahed Chowdhury Omi <shahedchowdhury120@gmail.com>
2025-09-19 19:25:27 +0600

    needs check

--- Problem Solving/Leetcode/189.py
+++ Problem Solving/Leetcode/189.py
@@ -1,33 +1,6 @@
 def rotate(k, nums):
 
-        arrLen = len(nums)
-
-        if arrLen > 1 and k > 0:
-
-            isEven = arrLen % 2 == 0
-
-            if isEven or arrLen < 5:
-                l = arrLen-k
-                m = l
-            else:
-                l = arrLen-1-k
-                m = l+1
-
-            v = nums[l]
-
-            nums[l] = nums[0]
-
-            if isEven: nums[0] = v
-            else: nums[0] = nums[m]
-
-            for i in range(1, l):
-                p = nums[i]
-                nums[i] = nums[m+i]
-
-                if(isEven): nums[m+i] = p
-                else: nums[m+i-1] = p
-            
-            if not isEven: nums[arrLen-1] = v
+        # Find an optimized solutions
 
         return nums          
 
---
+++ Problem Solving/Leetcode/55.py
@@ -0,0 +1,9 @@
+def canJump(nums):
+
+    # Follow greedy solution
+    return
+
+
+print(canJump([2,3,1,1,4]))
+print(canJump([3,2,1,0,4]))
+print(canJump([1]))
