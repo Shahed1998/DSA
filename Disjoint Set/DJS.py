1a19a86f29edc98c45208067289173cbbb865eff
shahed.omi <shahed.omi@datasoft-bd.com>
2026-03-24 17:21:07 +0600

    understand flattening of array

---
+++ Disjoint Set/DJS.py
@@ -0,0 +1,41 @@
+class Solution:
+    def __init__(self, n):
+        self.djs = [i for i in range(n)]
+
+    # Without path compression
+    # def find(self, x):
+    #     if self.djs[x] != x: 
+    #         return self.find(self.djs[x])
+    #     return x
+    
+    # With path compression / flattening
+    def find(self, x):
+        if self.djs[x] != x:
+            self.djs[x] = self.find(self.djs[x])  # flatten
+        return self.djs[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+
+        print(rootX, rootY)
+        print(self.djs)
+
+        if(rootX != rootY):
+            self.djs[rootY] = rootX
+        else:
+            print("Cycle detected")
+
+    def Print(self):
+        print(self.djs)
+
+            
+
+s = Solution(4)
+# print(s.find(3))
+s.union(0,1)
+s.union(1,2)
+# s.union(2,3)
+# s.union(3,0)
+# s.Print()
+
