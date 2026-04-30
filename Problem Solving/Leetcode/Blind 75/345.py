3161e9f62d299445150d1efae9bec3505b996195
Shahed Chowdhury Omi <shahedchowdhury120@gmail.com>
2026-04-30 02:45:39 +0100

    solved leetcode 345.py

---
+++ Problem Solving/Leetcode/Blind 75/345.py
@@ -0,0 +1,33 @@
+class Solution:
+    def reverseVowels(self, s):
+
+        vowels = set("AEIOUaeiou")
+        lptr = 0
+        rptr = len(s)-1
+        s_list = list(s)
+        
+
+        while lptr < rptr:
+
+            if s_list[lptr] not in vowels:
+                lptr += 1
+            
+            elif s_list[rptr] not in vowels:
+                rptr -= 1
+            
+            else:
+                s_list[lptr], s_list[rptr] = s_list[rptr], s_list[lptr]
+                lptr += 1
+                rptr -= 1
+
+        
+        return "".join(s_list)
+
+
+                
+
+
+
+s = Solution()
+s.reverseVowels(s = "IceCream")
+s.reverseVowels(s = "leetcode")
