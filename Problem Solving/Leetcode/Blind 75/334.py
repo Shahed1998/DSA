15faab4024009819d46f96c603aa80edc37d45d9
Shahed Chowdhury Omi <shahedchowdhury120@gmail.com>
2026-05-06 03:15:04 +0100

    LCB75 - 334.py

--- Problem Solving/Leetcode/Blind 75/334.py
+++ Problem Solving/Leetcode/Blind 75/334.py
@@ -1,45 +1,20 @@
 class Solution:
     def increasingTriplet(self, nums):
-        result = False
-
-        i = 0
-        iVal = nums[0]
-        iIndex = 0
-
-        while i < len(nums)-2:
-            
-            if iVal > nums[i]:
-                iVal = nums[i]
-                iIndex = i
-
-            i += 1
-
-        j = iIndex
-        jVal = -1
-        jIndex = -1
-
-        while j < len(nums)-1:
-            if iVal < nums[j]:
-                jVal = nums[j]
-                jIndex = j
-
-            j += 1
-
-
-        k = jIndex
-        kVal = -1
-        kIndex = -1
-
-        while k < len(nums):
-            if kVal < nums[k]:
-                kVal = nums[k]
-                kIndex = k
-
-            k += 1
         
+        first = float('inf')
+        second = float('inf')
 
-        return (iIndex < jIndex < kIndex) and (iVal < jVal < kVal)
-            
+        for num in nums:
+            if num <= first:
+                first = num
+            elif num <= second:
+                second = num
+            else:
+                return True
+
+        return False 
+
+ 
 
 s = Solution()
-s.increasingTriplet([2,1,5,0,4,6])
+print(s.increasingTriplet([2147483649, 5, 2, 2147483647,1,4,2]))
