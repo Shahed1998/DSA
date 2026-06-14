d88e1520e0587de80a90eead1c4fa88d1453feab
Shahed Chowdhury Omi <shahedchowdhury120@gmail.com>
2026-06-14 03:48:55 +0100

    LCB75AS495

---
+++ Problem Solving/Leetcode/Blind 75/Array and String/495.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findPoisonedDuration(self, timeSeries, duration):
+
+        # Interval patterns
+        # Teemo Attacking (495)
+        # Merge Intervals (56)
+        # Insert Interval (57)
+        # Non-overlapping Intervals (435)
+        # Meeting Rooms / Meeting Rooms II
+
+
+            
+
+
+
+
+
+s = Solution()
+# print(s.findPoisonedDuration(timeSeries=[1,2,3,4,5,6,7,8,9], duration=0))
+# print(s.findPoisonedDuration(timeSeries = [1,4], duration = 2))
+# print(s.findPoisonedDuration(timeSeries = [1,2], duration = 2))
+s.findPoisonedDuration(timeSeries=[1,2,3,4,5,6,7,8,9], duration=100000)
+        
